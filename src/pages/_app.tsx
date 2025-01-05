d79ba6d549060c4046d15e5a5bd6ef51018efa86
Amos <nie21.kjma@e.ntu.edu.sg>
2025-01-05 19:57:12 +0800

    analytics 9th commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
-import {Analytics} from '@vercel/analytics/react';
+
 import type {AppProps} from 'next/app';
 import {memo} from 'react';
+import {Analytics} from '@vercel/analytics/react';
 
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
+      <Analytics/>
     </>
   );
 });
