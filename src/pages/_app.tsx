f1c93911cb048e36df23f78507d08ef159b97ed2
Khan Jun Ming, Amos <101790228+amoskhan@users.noreply.github.com>
2025-07-24 13:30:41 +0800

    speed insight addition
    speed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
 
 import {Analytics} from '@vercel/analytics/react';
+import {SpeedInsights} from '@vercel/speed-insights/next'; // <-- Add this import
 import type {AppProps} from 'next/app';
 import {memo} from 'react';
 
@@ -10,6 +11,7 @@ const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 });
